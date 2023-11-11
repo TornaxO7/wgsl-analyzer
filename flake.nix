f10941a7fb3e15428722c0852ef5a227dc3d864b
TornaxO7 <tornax@proton.me>
2023-11-11 21:33:02 +0100

    flake: removing flake-utils

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -8,24 +7,26 @@
     };
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, naersk }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ rust-overlay.overlays.default ];
-        rust = pkgs.rust-bin.stable.latest.default;
-        builder = pkgs.callPackage naersk { rustc = rust; };
-        pkgs = import nixpkgs { inherit system overlays; };
+  outputs = { self, nixpkgs, rust-overlay, naersk }:
+    let
+      system = "x86_64-linux";
 
-      in rec {
-        packages.default = builder.buildPackage {
-          name = "wgsl-analyzer";
-          src = ./.;
-          cargoBuildOptions = opts: [ "-p" "wgsl_analyzer" ] ++ opts;
-        };
-        overlays.default = (self: super: { wgsl-analyzer = packages.default; });
+      overlays = [ rust-overlay.overlays.default ];
+      rust = pkgs.rust-bin.stable.latest.default;
+      builder = pkgs.callPackage naersk { rustc = rust; };
+      pkgs = import nixpkgs { inherit system overlays; };
+    in
+    rec {
+      packages.default = builder.buildPackage {
+        name = "wgsl-analyzer";
+        src = ./.;
+        cargoBuildOptions = opts: [ "-p" "wgsl_analyzer" ] ++ opts;
+      };
 
-        apps.default = packages.default;
-        devShell =
-          pkgs.mkShell { packages = with pkgs; [ rust-analyzer rust ]; };
-      });
+      overlays.default = (final: prev: { wgsl-analyzer = packages.default; });
+
+      apps.default = packages.default;
+      devShell =
+        pkgs.mkShell { packages = with pkgs; [ rust-analyzer rust ]; };
+    };
 }
